bdb8b8d8660d2174d5be425c04e1452cca18db59
voidalzk <116585234+voidalzk@users.noreply.github.com>
2023-10-03 11:00:48 -0300

    ex001

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+     <h1>
+        <?php 
+            echo "Olá, mundo! \u{1F30E}";
+          
+        ?>
+        </h1>
+    <p></p>
+    
+  </body>
+</html>
